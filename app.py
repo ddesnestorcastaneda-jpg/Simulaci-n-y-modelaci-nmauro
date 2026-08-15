1de5f24ccde3f7da980a162f4462536f302de136
ddesnestorcastaneda-jpg <d.des.nestor.castaneda@cali.edu.co>
2026-08-15 10:22:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(page_title="Curso de Simulación", page_icon="🎓")
+
+st.title("🎓 Portal del Curso de Simulación")
+st.write("¡Bienvenido! Selecciona la bitácora que deseas trabajar en el **menú lateral izquierdo**.")
